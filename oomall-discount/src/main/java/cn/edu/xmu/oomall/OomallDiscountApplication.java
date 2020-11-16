dac73032793b3a5ffa45ccef7b8a7fb0e60b5a59
abee <yaoxincong@outlook.com>
2020-11-16 18:51:38 +0800

    新增折扣模块

---
+++ oomall-discount/src/main/java/cn/edu/xmu/oomall/OomallDiscountApplication.java
@@ -0,0 +1,17 @@
+package cn.edu.xmu.oomall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xincong yao
+ * @date 2020-11-16
+ */
+@SpringBootApplication
+public class OomallDiscountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OomallDiscountApplication.class, args);
+	}
+
+}
---
+++ oomall-discount/src/test/java/cn/edu/xmu/oomall/OomallDiscountApplicationTest.java
@@ -0,0 +1,16 @@
+package cn.edu.xmu.oomall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xincong yao
+ * @date 2020-11-16
+ */
+@SpringBootApplication
+public class OomallDiscountApplicationTest {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OomallDiscountApplicationTest.class, args);
+	}
+}
