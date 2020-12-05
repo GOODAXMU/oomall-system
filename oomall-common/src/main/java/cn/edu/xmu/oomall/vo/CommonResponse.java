05c33b6fc5c8ef5a3f3d3aebdb5392a1c9565251
abee <yaoxincong@outlook.com>
2020-12-05 10:24:42 +0800

    更改通用返回对象的Json序列化规则

--- oomall-common/src/main/java/cn/edu/xmu/oomall/vo/CommonResponse.java
+++ oomall-common/src/main/java/cn/edu/xmu/oomall/vo/CommonResponse.java
@@ -1,5 +1,6 @@
 package cn.edu.xmu.oomall.vo;
 
+import com.fasterxml.jackson.annotation.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,8 @@ import java.io.Serializable;
  * @author xincong yao
  * @date 2020-10-26
  */
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@JsonPropertyOrder({"errno","errmsg","data"})
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -22,5 +25,33 @@ public class CommonResponse<T> implements Serializable {
 	public CommonResponse(Integer code, String message) {
 		this.code = code;
 		this.message = message;
+	}
+
+	@JsonGetter(value = "errno")
+	public Integer getCode() {
+		return code;
+	}
+
+	@JsonGetter(value = "errmsg")
+	public String getMessage() {
+		return message;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	@JsonSetter(value = "errno")
+	public void setCode(Integer code) {
+		this.code = code;
+	}
+
+	@JsonSetter(value = "errmsg")
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public void setData(T data) {
+		this.data = data;
 	}
 }
