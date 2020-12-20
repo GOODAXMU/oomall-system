cee59101c5649aafcfadb26b77740ed971de004b
abee <yaoxincong@outlook.com>
2020-12-20 18:22:20 +0800

    增加ShopDto字段非空判断

--- oomall-order/src/main/java/cn/edu/xmu/oomall/controller/CustomerOrderController.java
+++ oomall-order/src/main/java/cn/edu/xmu/oomall/controller/CustomerOrderController.java
@@ -21,7 +21,6 @@ import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
